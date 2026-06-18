9e63d46df37ab73743a15d66ac037f2498ec35a7
Arun-Aviar <arun.a@aviartechservices.com>
2026-06-18 16:59:52 +0530

    Referal Route remove user

--- src/controller/referral.js
+++ src/controller/referral.js
@@ -1,4 +1,4 @@
-import { getReferralService, updateService } from '../service/referral.js'
+import { getReferralService, updateService, removeService } from '../service/referral.js'
 
 
 export const getReferrals = async (req, res) => {
@@ -25,6 +25,22 @@ export const updateReferrals = async (req, res) => {
         console.log(data,'data')
 
         const approvedUser = await updateService(id, data);
+        res.status(200).json({
+            status: true,
+            data: approvedUser
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
+
+export const removeReferrals = async (req, res) => {
+    try {
+        const { id } = req.params
+        const approvedUser = await removeService(id);
         res.status(200).json({
             status: true,
             data: approvedUser
