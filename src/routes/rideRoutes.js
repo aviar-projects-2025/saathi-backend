d8cec25dae3b7643c9ecb5a0462c43a8e77a65ee
Preetha1408-T <preetha.t@aviartechservices.com>
2026-06-29 23:58:27 +0530

    Update rideRoutes.js

--- src/routes/rideRoutes.js
+++ src/routes/rideRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { createRide, getRides, editRide, checkActiveRide,deleteRide } from '../controller/ride.js'
+import { createRide, getRides, editRide, checkActiveRide } from '../controller/ride.js'
 
 const router = express.Router();
 
 router.get("/get", getRides);
 router.post("/", createRide);
 router.patch("/edit/:id", editRide);
-router.delete("/",deleteRide);
+// router.delete("/",deleteRide);
 router.get("/active/:userId", checkActiveRide);
 // router.delete("/:id");
 
