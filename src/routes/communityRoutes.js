cc8d07dfe28842faf741dcebdb524e6da8c20e3d
Preetha1408 <preetha.t@aviartechservices.com>
2026-07-08 16:20:53 +0530

    community-edit

--- src/routes/communityRoutes.js
+++ src/routes/communityRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import multer from 'multer'
-import { createPost,getPosts} from '../controller/community.js';
+import { createPost, getPosts, deletePost, editPost } from '../controller/community.js';
 import { getUploadSignature } from '../../config/cloudinary.js';
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/',getPosts)
-router.post('/',upload.single("postImage"), createPost)
-
-// cloudinary image
+router.get('/', getPosts)
+router.post('/', upload.single("postImage"), createPost)
+router.delete('/:postId', deletePost)
+// router.put("/:postId", editPost);
+router.put("/:postId", upload.single("postImage"), editPost);
 router.get("/upload-signature", getUploadSignature);
 
 export default router
