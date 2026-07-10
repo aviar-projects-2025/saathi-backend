406e44280d0bd2db7f701b117bcc1c0adfa1f2a4
Arun-Aviar <arun.a@aviartechservices.com>
2026-07-10 14:06:40 +0530

    notification and updation

---
+++ src/crons/rideStatus.job.js
@@ -0,0 +1,69 @@
+import cron from "node-cron";
+import Ride from "../model/ride.js";
+import { emitNotification } from "../../socket.js";
+import BookRide from "../model/bookride.js";
+import Notification from "../model/notification.js";
+
+cron.schedule("* * * * *", async () => {
+    const now = new Date();
+    console.log("Cron running at:", now);
+
+    // Step 1: find rides
+    const rides = await Ride.find({
+        travelStatus: "Waiting",
+        startTime: { $lte: now },
+    });
+
+    console.log(rides, 'rides')
+
+    if (!rides.length) return;
+
+    for (const ride of rides) {
+        const exists = await Notification.findOne({
+            userId: ride.createdBy,
+            type: "ride_started",
+            "data.rideId": ride._id,
+        });
+
+        console.log(exists, 'exists')
+
+        if (!exists) {
+            await Notification.create({
+                userId: ride.createdBy,
+                type: "ride_started",
+                message: "Looks like you have started your ride, confirm!",
+                data: {
+                    rideId: ride._id,
+                    from: ride.from,
+                    destination: ride.destination,
+                },
+                isRead: false,
+            });
+        }
+
+        emitNotification(ride.createdBy, {
+            type: "ride_started",
+            message: "Looks like you have started your ride, confirm!",
+            ride: {
+                _id: ride._id,
+                from: ride.from,
+                destination: ride.destination,
+                startTime: ride.startTime,
+                modeOfTravel: ride.modeOfTravel,
+            },
+            data: { rideId: ride._id },
+        });
+    }
+
+    // Step 2: update rides (VERY IMPORTANT)
+    // await Ride.updateMany(
+    //     { _id: { $in: rides.map(r => r._id) } },
+    //     { $set: { travelStatus: "Started" } }
+    // );
+
+    // // Step 3: reject pending bookings
+    // await BookRide.updateMany(
+    //     { rideId: { $in: rides.map(r => r._id) }, status: "PENDING" },
+    //     { status: "AUTO_REJECTED" }
+    // );
+});
