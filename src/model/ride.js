8e26109eca7dd18e9fa049891ece4d4c21e4d04b
SakthivelAyyanar <sakthivel.a@aviartechservices.com>
2026-09-09 09:31:45 +0530

    Update ride.js

--- src/model/ride.js
+++ src/model/ride.js
@@ -113,7 +113,7 @@ const rideSchema = new mongoose.Schema(
         "Student travel companion",
         "Women-only companion",
         "Family companion",
-        "",
+        "Regular",
       ],
       required: function () {
         return this.modeOfTravel === "Flight";
