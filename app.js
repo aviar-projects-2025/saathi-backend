e998c4bc0ee4e2a93fcbb59d86e0b9ac61c5aed2
Preetha1408 <preetha.t@aviartechservices.com>
2026-09-17 10:17:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const app = express();
 
 app.use(express.json());
 const allowedOrigins = [
-    // "http://localhost:5173",
-    // "https://saathi-frontend-sl8k.vercel.app",
+    "http://localhost:5173",
+    "https://saathi-frontend-sl8k.vercel.app",
     "https://saathirides.net"
 ];
 
